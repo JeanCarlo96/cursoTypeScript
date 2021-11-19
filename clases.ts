db30cbe7a8b2a2efff6ae8de63b9e05eb5809a7f
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-11-19 14:41:15 -0500

    JCMA Clases

---
+++ clases.ts
@@ -0,0 +1,25 @@
+/* Clases */
+
+class Rectangulo {
+    ancho: number
+    alto:number
+
+    constructor(ancho: number, alto: number) {
+        console.log('Constructor')
+        this.ancho = ancho
+        this.alto = alto
+    }
+
+    area() {
+        return this.ancho * this.alto
+    }
+
+    perimetro (){
+        return this.alto*2 + this.ancho*2
+    }
+}
+
+let r1 = new Rectangulo(5, 10)
+console.log(r1.area())
+let r2 = new Rectangulo(10, 15)
+console.log(r2.area())
