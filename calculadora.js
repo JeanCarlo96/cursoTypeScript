b494e7f4556b7a60dada8493753923a353e40ec9
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-11-19 13:03:28 -0500

    JCMA funciones

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+function sumar(n, m) {
+    return n + m;
+}
+console.log('El resultado de la suma es: ' + sumar(10, 15));
+var resta = function resta(n, m) {
+    return n - m;
+};
+console.log('El resultado de la resta es: ' + resta(10, 15));
+var multiplicacion = function (n, m) { return n * m; };
+console.log('El resultado de la multiplicación es: ' + multiplicacion(3, 6));
---
+++ calculadora.ts
@@ -0,0 +1,15 @@
+function sumar(n: number, m: number): number{
+    return n + m;
+}
+
+console.log('El resultado de la suma es: ' + sumar(10, 15))
+
+let resta = function resta(n: number, m: number) {
+    return n - m
+}
+
+console.log('El resultado de la resta es: ' + resta(10, 15));
+
+let multiplicacion = (n: number, m: number) => n * m
+
+console.log('El resultado de la multiplicación es: ' + multiplicacion(3, 6))
